de9dbc9e0285170dd084ff0f2a9fcb87e340942d
jbyers2 <jbyers2@gmail.com>
2016-07-12 13:23:22 -0500

    revised

---
+++ Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//24-hour clock
+var s=0
+var m=0
+var h=0
+for h in 0...23{
+    for m in 0...59{
+        for s in 0...59{
+        print("h = \(h), m=\(m), s=\(s)")
+            //delay determined by trial and error
+            for d in 1...70000000{
+                if d>0{
+                }
+            }
+        }
+    }
+}
